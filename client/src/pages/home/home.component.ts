9345e3e9af89d941899456ca5da163b6214be4c8
ValentinoBorgo <borgovalentino1@gmail.com>
2024-03-13 10:47:43 -0300

    Add impl of clasificacionService

--- client/src/pages/home/home.component.ts
+++ client/src/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClasificacionServiceService } from 'src/services/clasificacion/clasificacion-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private clasificacionService : ClasificacionServiceService){}
+
+  cla(){
+    this.clasificacionService.getClasification().subscribe(
+      clasificacion => {
+        console.log(clasificacion);
+      },
+      error => {
+        console.log("Error al obtener la clasificación : ",error);
+      }
+    )
+  }
 
 }
---
+++ client/src/services/clasificacion/clasificacion.ts
@@ -0,0 +1,6 @@
+export interface Clasification{
+    nombre : string,
+    puntos : Int16Array,
+    golesAFavor : Int16Array,
+    golesEnContra : Int16Array
+}
