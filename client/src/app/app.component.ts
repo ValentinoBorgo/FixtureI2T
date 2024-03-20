335ef6a630af237b35d1aee06bc6714deaae512a
ValentinoBorgo <borgovalentino1@gmail.com>
2024-03-20 11:05:56 -0300

    Add material ui to the project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,26 +10,9 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'FIXTURE';
 
-  //ARREGLAR ESTO
-
   @ViewChild('app-footer') appFooter!: ElementRef;
 
   constructor(private renderer: Renderer2, private router:Router) {}
-
-  /*
-  ngAfterViewInit() {
-    // Acceder al elemento app-footer
-    const footerElement = this.appFooter.nativeElement;
-
-    // Acceder a un elemento HTML dentro de app-footer (por ejemplo, un div con la clase "footer-content")
-    const innerElement = footerElement.querySelector('.footer');
-
-    console.log(innerElement);
-
-    // Realizar operaciones en el elemento innerElement, como ajustar el estilo
-    this.renderer.setStyle(innerElement, 'margin-left', '0px');
-  }
-  */
 
   viewRoute(): boolean {
     // Verifica si la URL actual coincide con la ruta específica
--- client/src/components/register/register.component.ts
+++ client/src/components/register/register.component.ts
@@ -3,11 +3,13 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { RegisterService } from 'src/services/register/register.service';
 import { registerBody } from 'src/services/register/registerBody';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
+
 export class RegisterComponent implements OnInit{
 
   msgError : boolean | null | undefined;
